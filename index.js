cba48785fecea755d7580b6a48b92a1a27844df0
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-13 18:35:41 +0100

    Index updated with a few queries.

--- index.js
+++ index.js
@@ -12,6 +12,41 @@ connection.connect(function (err) {
   console.log('Connected.');
 })
 
+
+// const instructor = { name: 'Stuart Kirby', city: 'Manchester' };
+// connection.query('INSERT INTO instructors SET ?', instructor, (err, res) => {
+//   if (err) throw err;
+//   console.log('Last insert ID', res.insertId);
+
+// })
+
+connection.query(
+  'UPDATE instructors SET city = ? WHERE ID = ?',
+  ['Chester', 1],
+  (err, result) => {
+    if (err) throw err;
+    console.log(`Changed ${result.changedRows} row(s)`);
+  }
+)
+
+connection.query('SELECT * FROM instructors', (err, rows) => {
+  if (err) throw err;
+
+  console.log('Data received from DB');
+  // console.log(rows);
+  rows.forEach((row) => {
+    console.log(`${row.name} teaches in ${row.city}`);
+  });
+
+})
+// connection.query(
+//   'DELETE FROM instructors WHERE id >= 5', [5], (err, result) => {
+//     if (err) throw err;
+//     console.log(`Seleted ${result.affectedRows} row(s)`);
+//   }
+// )
+
+
 connection.end((err) => {
   console.log('Ended.')
 })
